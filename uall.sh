f1d19f353d30b4b51c140bca6ab6c1d3517cbf7b
Gurjiwan Singh <gurrjiwan@gmail.com>
2023-08-15 22:25:12 +0530

    Initial Release

---
+++ uall.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Script to run all update commands
+
+# Using \033[{ANSI escae code}m format to define colors for printf.
+# Similar to how \n is used for new line \033[ is used fro ANSI colors
+# some ANSI codes are [RED] 0;31 [Blue] 0;34 [Yellow] 1;33 [Cyan] 0;36
+
+Cyan='\033[0;36m' # Simple shell variables to hold color values
+Yellow='\033[1;33m' # shell variables can be called by ${Variable name}
+NC='\033[0m' # No color
+
+printf "${Yellow} Running ${NC}pacman -Syu \n"
+sudo pacman -Syu
+printf "${Cyan} ---	END	---\n" 
+printf "${Yellow} Running ${NC}yay -Sua \n"
+yay -Sua
+printf "${Cyan} ---	END	---\n" 
+printf "${Yellow} Running ${NC}flatpak update......\n"
+flatpak update
+printf "${Cyan} ---	END	---\n" 
